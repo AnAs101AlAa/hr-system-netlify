aaa7aedea85c621355b543d63a390831a8fd5983
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2025-09-07 00:40:08 +0300

    fix: adjust positioning of Navbar and add padding to main content

--- src/components/hoc/WithNavbar.tsx
+++ src/components/hoc/WithNavbar.tsx
@@ -4,10 +4,10 @@ import MobileNavbar from "../navbar/MobileNavbar";
 const WithNavbar = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="relative">
-      <div className="md:block hidden">
+      <div className="md:block hidden fixed top-4 w-full">
         <Navbar />
       </div>
-      <main>{children}</main>
+      <main className="pt-20">{children}</main>
       <footer className="md:hidden block fixed bottom-0 w-full">
         <MobileNavbar />
       </footer>
