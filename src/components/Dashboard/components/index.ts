4bd9d9f084e9aee5f70b29d68cf8de81f05fed6a
hazemyasser123 <hazemyasser6@gmail.com>
2025-09-07 15:56:23 +0300

    Add EventCard component to dashboard
    Introduces the EventCard component for displaying event details and actions. Updates the index file to export EventCard for use in other parts of the dashboard.

--- src/components/Dashboard/components/index.ts
+++ src/components/Dashboard/components/index.ts
@@ -2,5 +2,6 @@ import WelcomeCard from "./WelcomeCard";
 import ActionCards from "./ActionCards";
 import Pagination from "./Pagination";
 import EventsList from "./EventsList";
+import EventCard from "./EventCard";
 
-export { WelcomeCard , ActionCards, Pagination, EventsList };
+export { WelcomeCard , ActionCards, Pagination, EventsList, EventCard };
