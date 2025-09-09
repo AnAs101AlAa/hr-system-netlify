1e6e3fbf038634b60451aabed7e06ba90a8cbc27
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2025-09-09 12:50:12 +0300

    fix: replace div with NavLink for Events card to enable navigation

--- src/components/dashboard/actionCards.tsx
+++ src/components/dashboard/actionCards.tsx
@@ -1,4 +1,5 @@
 import { MdQrCodeScanner, MdEvent } from "react-icons/md";
+import { NavLink } from "react-router-dom";
 
 const ActionCards = () => {
   return (
@@ -12,14 +13,17 @@ const ActionCards = () => {
         </div>
       </div>
 
-      <div className="flex-1 h-[80px] sm:h-[100px] md:h-[110px] bg-white border border-dashboard-card-border shadow-md rounded-[16px] flex flex-col items-center justify-center gap-1 p-2 cursor-pointer">
+      <NavLink
+        to={"/events"}
+        className="flex-1 h-[80px] sm:h-[100px] md:h-[110px] bg-white border border-dashboard-card-border shadow-md rounded-[16px] flex flex-col items-center justify-center gap-1 p-2 cursor-pointer"
+      >
         <div className="text-dashboard-icon text-[26px] sm:text-[32px] md:text-[35px]">
           <MdEvent />
         </div>
         <div className="text-dashboard-card-text font-bold text-[11px] sm:text-[12px] md:text-[13px] leading-[10px] md:leading-[14px] font-inter text-center">
           Events
         </div>
-      </div>
+      </NavLink>
     </div>
   );
 };
