1e7c96302d96ec067e674137d2db4fd2b4d8e473
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2025-09-09 12:46:49 +0300

    fix: update import path for EventDetails component to reflect new directory structure

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,5 @@
 import LoginPage from "./auth/LoginPage";
 import HomePage from "./homePage/DashboardPage";
-import EventDetails from "./events/EventDetails";
+import EventDetails from "./events/event-details/EventDetails";
 
 export { LoginPage, HomePage , EventDetails };
