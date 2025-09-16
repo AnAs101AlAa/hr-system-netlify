9d9306bd7e108e7c7fe794f63542f767b26183ae
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2025-09-16 21:38:52 +0300

    undone the qr page disables

--- src/components/dashboard/EventCard.tsx
+++ src/components/dashboard/EventCard.tsx
@@ -5,7 +5,7 @@ import Button from "@/components/generics/Button";
 import { ButtonTypes, ButtonWidths } from "@/constants/presets";
 import type { Event } from "@/types/event";
 import QRScannerModal from "@/components/scan_qr/QRScannerModal";
-import  format from "@/utils/Formater";
+import format from "@/utils/Formater";
 import { IoIosPin } from "react-icons/io";
 
 const EventCard: React.FC<{ event: Omit<Event, "attendees"> }> = ({
@@ -14,37 +14,66 @@ const EventCard: React.FC<{ event: Omit<Event, "attendees"> }> = ({
   const { id, title, startDate, endDate, location } = event;
   const navigate = useNavigate();
   const [isQRModalOpen, setIsQRModalOpen] = useState(false);
-
-  // if event in the past, only show "Details" button, else show both "Details" and "Scan QR" buttons
+  console.log(event);
   const isPastEvent = new Date(endDate) < new Date();
+  // Check if event is happening today and start time has come or passed, and hasn't ended yet
+  const now = new Date();
+  const eventStart = new Date(startDate);
+  const eventEnd = new Date(endDate);
+  const isEventTodayAndStarted =
+    eventStart.toDateString() === now.toDateString() &&
+    now >= eventStart &&
+    now < eventEnd;
 
   return (
     <div className="bg-white rounded-xl shadow-md p-5 flex flex-col gap-4 h-full relative">
       <div className="flex items-center gap-3">
-        <h3 className="font-bold text-gray-800 text-[20px] md:text-[22px] lg:text-[24px]">{title}</h3>
+        <h3 className="font-bold text-gray-800 text-[20px] md:text-[22px] lg:text-[24px]">
+          {title}
+        </h3>
       </div>
 
       <div className="flex flex-col text-sm text-gray-500 gap-1 text-[13px] md:text-[14px] lg:text-[15px]">
-        <span className="flex gap-1 items-center"><FaCalendarAlt className="text-primary text-md mr-1" /><span className="font-semibold">Start Date:</span> {format(startDate, "full")}</span>
-        <span className="flex gap-1 items-center"><FaCalendarAlt className="text-primary text-md mr-1" /><span className="font-semibold">End Date:</span> {format(endDate, "full")}</span>
-        <span className="flex gap-1"><IoIosPin className="text-primary size-5 -ml-0.5" /><span className="font-semibold">Location:</span> {location}</span>
+        <span className="flex gap-1 items-center">
+          <FaCalendarAlt className="text-primary text-md mr-1" />
+          <span className="font-semibold">Start Date:</span>{" "}
+          {format(startDate, "full")}
+        </span>
+        <span className="flex gap-1 aitems-center">
+          <FaCalendarAlt className="text-primary text-md mr-1" />
+          <span className="font-semibold">End Date:</span>{" "}
+          {format(endDate, "full")}
+        </span>
+        <span className="flex gap-1">
+          <IoIosPin className="text-primary size-5 -ml-0.5" />
+          <span className="font-semibold">Location:</span> {location}
+        </span>
       </div>
 
       <div className="flex justify-end items-center gap-3 mt-1 absolute bottom-4 right-5 w-auto">
         {/* you can later adjust the onClick for the detail and scanQR however you want */}
-        <Button
-          buttonText="Details"
-          onClick={() => navigate(`/events/${id}`)}
-          type={ButtonTypes.SECONDARY}
-          width={ButtonWidths.AUTO}
-        />
-        {!isPastEvent && (
+        {isPastEvent && (
+          <Button
+            buttonText="Details"
+            onClick={() => navigate(`/events/${id}`)}
+            type={ButtonTypes.SECONDARY}
+            width={ButtonWidths.AUTO}
+          />
+        )}
+
+        {isEventTodayAndStarted && (
           <Button
             buttonText="Scan QR"
             onClick={() => setIsQRModalOpen(true)}
             type={ButtonTypes.PRIMARY}
             width={ButtonWidths.AUTO}
           />
+        )}
+        {/* fallback */}
+        {!isPastEvent && !isEventTodayAndStarted && (
+          <span className="bg-gray-100 text-gray-500 px-3 py-1 rounded-md text-xs">
+            see you soon
+          </span>
         )}
       </div>
 
--- src/components/dashboard/actionCards.tsx
+++ src/components/dashboard/actionCards.tsx
@@ -1,10 +1,10 @@
-import { MdEvent } from "react-icons/md";
+import { MdQrCodeScanner, MdEvent } from "react-icons/md";
 import { NavLink } from "react-router-dom";
 
 const ActionCards = () => {
   return (
     <div className="flex flex-row justify-between gap-5 mx-auto">
-      {/* <NavLink
+      <NavLink
         to={"/events/scan-qr"} className="flex-1 h-[80px] sm:h-[100px] md:h-[110px] bg-white border border-dashboard-card-border shadow-md rounded-[16px] flex flex-col items-center justify-center gap-1 p-2 cursor-pointer">
         <div className="text-dashboard-icon text-[26px] sm:text-[32px] md:text-[35px]">
           <MdQrCodeScanner />
@@ -12,7 +12,7 @@ const ActionCards = () => {
         <div className="text-dashboard-card-text font-bold text-[11px] sm:text-[12px] md:text-[13px] leading-[10px] md:leading-[14px] font-inter text-center">
           Scan QR
         </div>
-      </NavLink> */}
+      </NavLink>
 
       <NavLink
         to={"/events"}
