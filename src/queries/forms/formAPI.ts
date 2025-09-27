06b2551956e474aae4802e175760cb9be6ce277d
AnAs101AlAa <anas.ibrahim03@eng-st.cu.edu.eg>
2025-09-28 01:53:54 +0300

    merged apis into main backend

--- src/queries/forms/formAPI.ts
+++ src/queries/forms/formAPI.ts
@@ -1,30 +1,30 @@
 import type { Answer } from "@/types/question";
-import { formsApi } from "../axiosInstance";
+import { systemApi } from "../axiosInstance";
 import type { serverRequestForm } from "@/types/form";
 
-const FORMS_API_URL = formsApi.defaults.baseURL + "/v1/Form";
+const FORMS_API_URL = systemApi.defaults.baseURL + "/v1/Form";
 
 export async function getForms() {
-    const response = await formsApi.get(`${FORMS_API_URL}`);
+    const response = await systemApi.get(`${FORMS_API_URL}`);
     return response.data;
 }
 
 export async function getForm(formId: string) {
-    const response = await formsApi.get(`${FORMS_API_URL}/${formId}`);
+    const response = await systemApi.get(`${FORMS_API_URL}/${formId}`);
     return response.data
 }
 
 export async function saveAnswer(formId: string, questionId: number, answer: Answer, formName: string) {
-    const response = await formsApi.post(`${FORMS_API_URL}${formId}/questions/${questionId}/answers`, { answer, formName });
+    const response = await systemApi.post(`${FORMS_API_URL}${formId}/questions/${questionId}/answers`, { answer, formName });
     return response.data;
 }
 
 export async function submitForm(formId: string, answers: string[]) {
-    const response = await formsApi.post(`${FORMS_API_URL}/${formId}/Response`, {response: answers})
+    const response = await systemApi.post(`${FORMS_API_URL}/${formId}/Response`, {response: answers})
     return response.data;
 }
 
 export async function createForm(formData: serverRequestForm) {
-    const response = await formsApi.post(`${FORMS_API_URL}`, formData);
+    const response = await systemApi.post(`${FORMS_API_URL}`, formData);
     return response.data;
 }
