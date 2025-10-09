d1150d6829c553449101b1910434ef612a3f6396
AnAs101AlAa <anas.ibrahim03@eng-st.cu.edu.eg>
2025-10-09 11:23:58 +0300

    try

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -11,24 +10,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://test-prod.runasp.net",
-        changeOrigin: true,
-        secure: true, // because target is https
-        // IMPORTANT: do NOT rewrite /api -> "" because your backend expects /api/...
-        // rewrite: (p) => p.replace(/^\/api/, ""), // <-- leave this OUT
-        configure: (proxy) => {
-          // optional: log what is actually proxied
-          proxy.on("proxyReq", (_proxyReq, req) => {
-            console.log("[proxy] =>", req.method, req.url);
-          });
-          proxy.on("proxyRes", (proxyRes, req) => {
-            console.log("[proxy] <=", proxyRes.statusCode, req.url);
-          });
-        },
-      },
-    },
-  },
-});
+})
