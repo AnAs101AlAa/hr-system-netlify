b943909fc17472931c1e349f993dea9a7c16104b
AnAs101AlAa <anas.ibrahim03@eng-st.cu.edu.eg>
2025-11-28 11:30:36 +0200

    new UI for dashboard page

--- src/features/dashboard/components/welcomeCard.tsx
+++ src/features/dashboard/components/welcomeCard.tsx
@@ -6,12 +6,9 @@ const WelcomeCard = () => {
   const { user } = useSelector((state: RootState) => state.auth);
 
   return (
-    <div className="mt-10 mb-6 w-auto bg-dashboard-welcome-bg border border-dashboard-border rounded-[7px] box-border px-6 py-4 relative overflow-hidden">
-      <div className="flex flex-row items-center gap-4 mb-3">
-        <div
-          className="w-[70px] h-[70px] bg-gray-300 rounded-full flex-shrink-0  sm:w-[80px] sm:h-[80px] md:w-[90px] md:h-[90px] lg:w-[100px] lg:h-[100px] xl:w-[110px] xl:h-[110px] flex items-center justify-center
-        overflow-hidden"
-        >
+    <div className="mt-4 w-full border border-dashboard-border rounded-[7px] box-border relative overflow-hidden">
+      <div className="flex flex-col items-center justify-center gap-2">
+        <div className="w-[100px] h-[100px] bg-gray-300 mb-3 rounded-full flex-shrink-0 sm:w-[100px] sm:h-[100px] md:w-[120px] md:h-[120px] lg:w-[130px] lg:h-[130px] xl:w-[140px] xl:h-[140px] flex items-center justify-center overflow-hidden">
           <LazyImageLoader
             src={
               user?.profileImageUrl ||
@@ -24,16 +21,13 @@ const WelcomeCard = () => {
           />
         </div>
         <div className="flex flex-col justify-center flex-1 gap-1">
-          <div className="text-dashboard-welcome-text font-normal text-[13.4px] leading-[15px] font-inter sm:text-[15px] sm:leading-[18px] md:text-[16px] md:leading-[19px] lg:text-[17px] lg:leading-[20px] xl:text-[18px] xl:leading-[21px]">
-            Welcome back,
+          <div className="text-center text-dashboard-welcome-text font-normal text-[13.4px] leading-[15px] font-inter sm:text-[15px] sm:leading-[18px] md:text-[16px] md:leading-[19px] lg:text-[17px] lg:leading-[20px] xl:text-[18px] xl:leading-[21px]">
+            Welcome back
           </div>
-          <div className="text-dashboard-user-name font-bold text-[19.7px] leading-[23px] font-inter sm:text-[21px] sm:leading-[24px] md:text-[22px] md:leading-[26px] lg:text-[24px] lg:leading-[28px] xl:text-[26px] xl:leading-[30px]">
-            {user?.name || "Logine Ahmed"}
+          <div className="text-dashboard-user-name font-bold text-[20px] leading-[23px] font-inter sm:text-[22px] sm:leading-[24px] md:text-[24px] md:leading-[26px] lg:text-[26px] lg:leading-[28px] xl:text-[28px] xl:leading-[30px]">
+            {user?.name}
           </div>
         </div>
-      </div>
-      <div className="text-dashboard-description font-normal text-[14.9px] leading-[20px] font-inter sm:text-[16px] sm:leading-[22px] md:text-[17px] md:leading-[23px] lg:text-[18px] lg:leading-[24px] xl:text-[19px] xl:leading-[25px]">
-        Your dashboard for managing event attendance efficiently.
       </div>
     </div>
   );
